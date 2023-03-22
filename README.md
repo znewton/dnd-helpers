403a1be2ae6eb736cc7b2915d8210ed08e7fe67b
Zach Newton <znewton@microsoft.com>
2023-03-22 13:04:09 -0700

    item types

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # D&D Helpers
 
-Some useful scripts for generating helpful D&D content in [obsidian.md](https://obsidian.md) vaults using [5e.tools](https://5e.tools)
+Some useful scripts for generating helpful D&D content in [obsidian.md](https://obsidian.md) vaults using [5e.tools](https://5e.tools).
+
+Specifically, these tools pull data from [5etools-mirror-1 Github Repo](https://github.com/5etools-mirror-1/5etools-mirror-1.github.io/tree/master/data), which is a fork of the source for [5e.tools](https://5e.tools) website with all of the raw data in JSON format, which is better for parsing than attempting to manually export the data from the website itself.
+
+## Getting Started
+
+### 1. Install Required Dependencies
+
+1. This is a [Typescript](https://www.typescriptlang.org/) project that runs using [Node.js](https://nodejs.org/en) runtime. Install Node.js using [NVM (Node Version Manager)](https://github.com/nvm-sh/nvm#installing-and-updating) then running `nvm use` within the root directory for this project.
+2. This project uses [Yarn](https://yarnpkg.com/) for dependency management. Install Yarn by following [Yarn's installation directions](https://yarnpkg.com/getting-started/install).
+3. Install NPM dependencies using `yarn install`
+
+### 2. Configure Source Material
+
+_**IMPORTANT:**_ Please only use this tool with source material _**that you legally own!**_. [5e.tools](https://5e.tools) is an awesome tool that is freely available for use, so please do not abuse it by using it to pirate from Wizards of the Coast.
+
+To configure which source books that you own,
+
+1. Run `yarn start list-books` to view all available source books and their corresponding abbreviations.
+2. Update the `ownedSourceBooks` property of the `config` variable exported from `src/config/config.ts` with the abbreviations of source books you own.
+
+To configure where the output goes, configure the appropriate properties within the `config` variable in `src/config/config.ts`.
+
+### 3. Generate ALL the Files
+
+Run `yarn start gen all` to generate all the helper files.
--- src/data/items.ts
+++ src/data/items.ts
@@ -1,7 +1,242 @@
-export interface IItem {
-  name: string;
+import { isOwned } from '../utils';
+import { Entry, FiveEToolsBasePath, getJsonData } from './common';
+
+export interface IItemProperty {
+  abbreviation: string;
+  source: string;
+  page: number;
+  entries: Entry[];
+  // TODO: these have special templates like {{prop_name}} and {{item.range}}
+  template: string;
 }
 
+export interface IItemType {
+  abbreviation: string;
+  source: string;
+  page: number;
+  name: string;
+  entries: Entry[];
+}
+export interface IItemTypeAdditionalEntries {
+  appliesTo: string;
+  source: string;
+  entries: Entry[];
+}
+
+export interface IItemEntry {
+  name: string;
+  source: string;
+  // TODO: these have special templates like {{item.detail1}} or {{item.resist}}
+  entriesTemplate: Entry[];
+}
+
+export interface IItemBase {
+  name: string;
+  source: string;
+  otherSources?: { source: string, page: number }[];
+  page: number;
+  type?: string;
+  /**
+   * Spellcasting Focus Type
+   */
+  scfType?: string;
+  rarity: string;
+  weight?: number;
+  value?: number;
+  weaponCategory?: string;
+  property: string[];
+  age?: string;
+  /**
+   * Weapons
+   */
+  range?: string;
+  dmg1?: string;
+  dmg2?: string;
+  dmgType?: string;
+  weapon?: boolean;
+  spear?: boolean;
+  firearm?: boolean;
+  sword?: boolean;
+  bow?: boolean;
+  /**
+   * Armor
+   */
+  ac?: number;
+  armor?: boolean;
+  stealth?: boolean;
+  strength?: string;
+  /**
+   * Sometimes like `arrow|phb`
+   */
+  ammoType?: string;
+  entries?: Entry[];
+  /**
+   * Sometimes like `sling bullet|phb`
+   */
+  packContents?: ({ item: string; quantity: number; } | string | { special: string; })[];
+}
+
+interface IModifySpeed {
+  equal?: {
+    // E.g. "fly": "walk" -> Fly speed becomes walk speed
+    [key: string]: string | number;
+  };
+  multiply?: {
+    // E.g. "walk": 2 -> walk speed multiplied by 2
+    [key: string]: string | number;
+  };
+  static?: {
+    // E.g. "walk": 30 -> walk speed becomes 30 ft
+    [key: string]: string | number;
+  };
+  bonus?: {
+    // E.g. "*": 5 -> all movement increased by 5
+    [key: string]: string | number;
+  };
+}
+type AbilityAbbrevs = {
+  con: 'constitution';
+  str: 'strength';
+  dex: 'dexterity';
+  int: 'intelligence';
+  wis: 'wisdom';
+  cha: 'charisma';
+};
+type AbilityModifier = {
+  // E.g. "con": 2 -> Constitution +2
+  [Property in keyof AbilityAbbrevs]: number;
+};
+type AbilityModifierChoice = {
+  // E.g. "from": ["dex", "str"], "count": 1, "amount": 2 -> Choose to increase 1 of dex or strength by 2
+  from: [keyof AbilityAbbrevs][];
+  count: number;
+  amount: 2;
+};
+
+interface IModifyAbility {
+  // E.g. "con": 19 -> constitution score becomes 19 (unless already higher)
+  static?: AbilityModifier;
+  choose?: AbilityModifierChoice;
+}
+
+interface IContainerCapacity {
+  weight?: number[];
+  weightless?: boolean;
+}
+
+export interface IItem extends IItemBase {
+  additionalSources?: { source: string; page: number; }[];
+  tier?: string;
+  reqAttune?: string | boolean;
+  // Sometimes like `artificer|tce`
+  reqAttuneTags?: {
+    class?: string;
+    background?: string;
+    alignment?: string;
+    creatureType?: string;
+  }[];
+  // Categories
+  wondrous?: boolean;
+  tattoo?: boolean;
+  curse?: boolean;
+  poison?: boolean;
+  sentient?: boolean;
+  staff?: boolean;
+  // Bonuses
+  bonusSpellAttack?: string;
+  bonusSaveDc?: string;
+  bonusWeapon?: string;
+  bonusWeaponAttack?: string;
+  bonusWeaponDamage?: string;
+  bonusAc?: string;
+  bonusSavingThrow?: string;
+  bonusWeaponCritDamage?: string;
+  critThreshold?: number;
+  grantsProficiency?: boolean;
+  modifySpeed?: IModifySpeed;
+  ability?: IModifyAbility | AbilityModifier | AbilityModifierChoice;
+  immune?: string[];
+  resist?: string[];
+  conditionImmune?: string[];
+  vulnerable?: string[];
+  // Other
+  containerCapacity: IContainerCapacity;
+  atomicPackContents?: boolean;
+  focus?: string[];
+  lootTables?: string[];
+  recharge?: string;
+  // Sometimes like {@dice 1d20}
+  rechargeAmount?: string | number;
+  charges?: number;
+  miscTags?: string[];
+  detail1?: string;
+  poisonTypes?: string[];
+  // If true, entries can have like {#itemEntry Absorbing Tattoo|TCE}
+  hasRefs?: boolean;
+  attachedSpells?: string[];
+  // Sometimes like `replicate magic item|tce`
+  optionalFeatures?: string[];
+  // Vehicles
+  crew?: number;
+  vehAc?: number;
+  vehHp?: number;
+  vehSpeed?: number;
+  vehDmgThresh?: number;
+  capPassenger?: number;
+  capCargo?: number;
+  seeAlsoVehicle?: string[];
+  // Extensions
+  baseItem?: string;
+  _copy: {
+    name: string;
+    source: string;
+    _preserve: { [key: string]: boolean | undefined };
+    _mod?: any; // TODO: look for `insertArr`
+  }; // TODO: these extend objects, but those objects themselves can be extended
+  // They are also included in ItemGroups
+}
+
+interface IItemGroup extends IItem {
+  // Like `Acid Absorbing Tattoo|TCE`
+  items: string[];
+}
+
+type ItemsBaseJson = {
+  item: IItem[];
+  itemProperty: IItemProperty[];
+  itemType: IItemType[];
+  itemEntry: IItemEntry[];
+  itemTypeAdditionalEntries: IItemTypeAdditionalEntries[];
+};
+const itemsBaseFilename = 'items-base.json';
+const itemsBaseBaseUrl = `${FiveEToolsBasePath}/data`;
+
+type ItemsJson = {
+  item: IItem[];
+  itemGroup: IItemGroup[];
+};
+const itemsFilename = 'items.json';
+const itemsBaseUrl = `${FiveEToolsBasePath}/data`;
+
 export async function listItems(): Promise<IItem[]> {
-  throw new Error('Not Implemented');
+  const itemsBaseJson = await getJsonData<ItemsBaseJson>(
+    itemsBaseFilename,
+    itemsBaseBaseUrl,
+    {
+      item: [], itemProperty: [], itemType: [], itemEntry: [], itemTypeAdditionalEntries: [],
+    },
+  );
+
+  const itemsJson = await getJsonData<ItemsJson>(
+    itemsFilename,
+    itemsBaseUrl,
+    {
+      item: [],
+      itemGroup: [],
+    },
+  );
+
+  // TODO: properties, types, additional entries, etc.
+  return [...itemsBaseJson.item, ...itemsJson.item]
+    .filter((item) => isOwned(item));
 }
